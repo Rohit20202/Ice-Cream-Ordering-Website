14b07495588fe528daaaec45b0abbc014294cd1a
Rohit Jaiswal <soulrohitx@gmail.com>
2026-03-14 21:11:21 +0530

    Add files via upload

---
+++ js/user_script.js
@@ -0,0 +1,69 @@
+let profile = document.querySelector('.header .flex .profile-detail');
+document.querySelector('#user-btn').onclick = () => {
+   profile.classList.toggle('active');
+   searchForm.classList.remove('active');
+};
+
+let searchForm = document.querySelector('.header .flex .search-form');
+document.querySelector('#search-btn').onclick = () => {
+   searchForm.classList.toggle('active');
+   profile.classList.remove('active');
+};
+
+let navbar = document.querySelector('.navbar');
+document.querySelector('#menu-btn').onclick = () => {
+   navbar.classList.toggle('active');
+};
+
+// Home slide
+window.addEventListener('DOMContentLoaded', function() {
+   var swiper = new Swiper(".home-slide", {
+      spaceBetween: 30,
+      centeredSlides: true,
+      autoplay: {
+         delay: 7500,
+         disableOnInteraction: false,
+      },
+      pagination: {
+         el: ".swiper-pagination",
+         clickable: true,
+      },
+      loop: true,
+   });
+});
+
+//testimonial
+const btn = document.getElementsByClassName('btn1');
+const slide = document.getElementById('slide');
+
+btn[0].onclick = function () {
+    slide.style.transform = 'translateX(0px)';
+    for (var i = 0; i < 4; i++) {
+        btn[i].classList.remove('active');
+    }
+    this.classList.add('active');
+};
+
+btn[1].onclick = function () {
+    slide.style.transform = 'translateX(-800px)';
+    for (var i = 0; i < 4; i++) {
+        btn[i].classList.remove('active');
+    }
+    this.classList.add('active');
+};
+
+btn[2].onclick = function () {
+    slide.style.transform = 'translateX(-1600px)';
+    for (var i = 0; i < 4; i++) {
+        btn[i].classList.remove('active');
+    }
+    this.classList.add('active');
+};
+
+btn[3].onclick = function () {
+    slide.style.transform = 'translateX(-2400px)';
+    for (var i = 0; i < 4; i++) {
+        btn[i].classList.remove('active');
+    }
+    this.classList.add('active');
+};
